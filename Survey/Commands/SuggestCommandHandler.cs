ea640869bb120ed642da3c9c314c821d5b274417
moeux <68925026+moeux@users.noreply.github.com>
2024-11-10 02:22:17 +0100

    Overhauled SuggestCommandHandler to conform to AutoCommand changes and removed icon saving.

--- Survey/Commands/SuggestCommandHandler.cs
+++ Survey/Commands/SuggestCommandHandler.cs
@@ -1,9 +1,7 @@
 using AutoCommand.Handler;
-using AutoCommand.Utils;
 using Discord;
 using Discord.WebSocket;
 using Serilog;
-using Serilog.Sinks.SystemConsole.Themes;
 using Survey.Database;
 using Survey.Database.Models;
 
@@ -11,110 +9,63 @@ namespace Survey.Commands;
 
 public class SuggestCommandHandler : ICommandHandler
 {
-    private const int MaxSize = 5 * 1024 * 1024;
-    private static readonly HttpClient HttpClient = new();
-    private readonly string _iconsPath;
-    private readonly ILogger _logger;
-
-    public SuggestCommandHandler(string iconsPath)
-    {
-        _iconsPath = iconsPath;
-        _logger = new LoggerConfiguration()
-            .Destructure.ByTransformingWhere<dynamic>(type => typeof(SocketUser).IsAssignableFrom(type),
-                user => new { user.Id, user.Username })
-            .Destructure.ByTransforming<SocketSlashCommand>(command => new { command.Id, command.CommandName })
-            .Enrich.FromLogContext()
-            .WriteTo.Console(
-                theme: AnsiConsoleTheme.Literate,
-                outputTemplate:
-                "[{Timestamp:HH:mm:ss} {Level:u3}] {Properties:j}{NewLine}{Message:lj}{NewLine}{Exception}")
-            .WriteTo.File(
-                EnvironmentUtils.GetVariable("ROCKET_LOG_FILE", "rocket-.log"),
-                outputTemplate:
-                "[{Timestamp:HH:mm:ss} {Level:u3}] {Properties:j}{NewLine}{Message:lj}{NewLine}{Exception}",
-                rollingInterval: RollingInterval.Day)
-            .CreateLogger()
-            .ForContext<SuggestCommandHandler>();
-
-        if (Directory.Exists(_iconsPath))
-        {
-            _logger.Information("Icons directory already existing, skipping creation");
-        }
-        else
-        {
-            var directoryInfo = Directory.CreateDirectory(_iconsPath);
-
-            if (directoryInfo.Exists)
-                _logger.Information("Created icons directory successfully");
-            else
-                _logger.Error("Failed to create icons directory");
-        }
-    }
-
     public string CommandName => "suggest";
-    public bool IsLongRunning => true;
 
-    public async Task<string> HandleAsync(SocketSlashCommand command)
+    public async Task HandleAsync(ILogger logger, SocketSlashCommand command)
     {
-        var logger = _logger.ForContext("Token", command.Token);
-        var response = "Your suggestion has been saved successfully, thank you!";
+        await command.DeferAsync(true);
+
         var options = command.Data.Options;
         var name = GetOption<string>(options, "name", ApplicationCommandOptionType.String);
         var note = GetOption<string>(options, "note", ApplicationCommandOptionType.String);
+        var icon = GetOption<IAttachment>(options, "icon", ApplicationCommandOptionType.Attachment);
         var minimum = GetOption<long>(options, "minimum", ApplicationCommandOptionType.Integer);
         var maximum = GetOption<long>(options, "maximum", ApplicationCommandOptionType.Integer);
-        var icon = GetOption<IAttachment>(options, "icon", ApplicationCommandOptionType.Attachment);
-        var id = Guid.NewGuid();
-        var path = icon is null ? null : Path.Combine(_iconsPath, id + Path.GetExtension(icon.Filename));
-        var suggestion = new Suggestion(id, name ?? string.Empty, note, path, minimum, maximum);
+        var userId = command.User.Id;
+        var suggestion = new Suggestion(name ?? string.Empty, note, icon?.Url, minimum, maximum, userId);
 
         logger.Information("User {@User} executed command {@Command} with suggestion {@Suggestion}",
             command.User, command, suggestion);
 
         if (string.IsNullOrWhiteSpace(name))
-            return "Please provide a name.";
+        {
+            await command.FollowupWithEmbed("Please provide a name.", Color.Red);
+            return;
+        }
 
         if (minimum <= 0)
-            return "Please provide a valid minimum amount of players required.";
+        {
+            await command.FollowupWithEmbed("Please provide a valid minimum amount of players required.", Color.Red);
+            return;
+        }
 
         if (maximum <= 0 || maximum < minimum || maximum >= 100)
-            return "Please provide a valid maximum amount of players possible.";
+        {
+            await command.FollowupWithEmbed("Please provide a valid maximum amount of players possible.", Color.Red);
+            return;
+        }
 
         if (DatabaseHelper.GetSuggestions().Contains(suggestion))
-            return "This suggestion has already been made.";
+        {
+            await command.FollowupWithEmbed("This suggestion has already been made.", Color.Red);
+            return;
+        }
 
         if (!DatabaseHelper.AddSuggestion(suggestion))
-            return "Something went wrong with saving your suggestion, please try again later.";
-
-        if (icon is { Size: <= MaxSize } && icon.ContentType.StartsWith("image") && path is not null)
         {
-            logger.Information("Saving image ({@ContentType}, {@Size} bytes) to {@Path}",
-                icon.ContentType, icon.Size, path);
-
-            await SaveImageAsync(icon.Url, path);
-        }
-        else
-        {
-            return "Your icon could not be saved, either because it is too big (>5MB) or not an image.";
+            await command.FollowupWithEmbed(
+                "Something went wrong while saving your suggestion, please try again later.", Color.Red);
+            return;
         }
 
-        return response;
+        await command.FollowupWithEmbed("Your suggestion has been saved successfully, thank you!", Color.Green);
     }
 
     private static T? GetOption<T>(
-        IEnumerable<SocketSlashCommandDataOption> options,
-        string name,
-        ApplicationCommandOptionType type)
+        IEnumerable<SocketSlashCommandDataOption> options, string name, ApplicationCommandOptionType type)
     {
         var option = Array.Find(options.ToArray(), option => option.Name == name && option.Type == type);
 
         return option?.Value is T value ? value : default;
-    }
-
-    private static async Task SaveImageAsync(string url, string path)
-    {
-        await using var imageStream = await HttpClient.GetStreamAsync(url);
-        await using var fileStream = File.Create(path);
-        await imageStream.CopyToAsync(fileStream);
     }
 }
