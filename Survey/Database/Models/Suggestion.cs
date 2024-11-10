16c0c8a4c8c45f9ba0f80cfd52e3b3e8faf18ded
moeux <68925026+moeux@users.noreply.github.com>
2024-11-10 02:20:55 +0100

    Added creation timestamp and author user ID to suggestion. Removed ID constructor parameter and equality operator overrides.

--- Survey/Database/Models/Suggestion.cs
+++ Survey/Database/Models/Suggestion.cs
@@ -2,9 +2,10 @@ using System.ComponentModel.DataAnnotations;
 
 namespace Survey.Database.Models;
 
-public class Suggestion(Guid id, string name, string? note, string? iconUrl, long minimum, long maximum)
+public class Suggestion(string name, string? note, string? iconUrl, long minimum, long maximum, ulong userId)
 {
-    public Guid Id { get; init; } = id;
+    public Guid Id { get; init; } = Guid.NewGuid();
+    public DateTimeOffset CreatedAt { get; init; } = DateTimeOffset.Now;
     [MaxLength(256)] public string Name { get; init; } = name;
 
     [MaxLength(256)] public string? Note { get; init; } = note;
@@ -14,27 +15,19 @@ public class Suggestion(Guid id, string name, string? note, string? iconUrl, lon
     public long Minimum { get; init; } = minimum;
     public long Maximum { get; init; } = maximum;
 
+    public ulong UserId { get; init; } = userId;
+
     public override bool Equals(object? obj)
     {
-        return obj is Suggestion other && (Id.Equals(other.Id) || (Name == other.Name &&
-                                                                   Note == other.Note &&
-                                                                   IconUrl == other.IconUrl &&
-                                                                   Minimum == other.Minimum &&
-                                                                   Maximum == other.Maximum));
+        return obj is Suggestion other &&
+               (Id.Equals(other.Id) || (string.Equals(Name, other.Name, StringComparison.InvariantCultureIgnoreCase) &&
+                                        string.Equals(Note, other.Note, StringComparison.InvariantCultureIgnoreCase) &&
+                                        Minimum == other.Minimum &&
+                                        Maximum == other.Maximum));
     }
 
     public override int GetHashCode()
     {
-        return HashCode.Combine(Id, Name, Note, IconUrl, Minimum, Maximum);
-    }
-
-    public static bool operator ==(Suggestion? left, Suggestion? right)
-    {
-        return Equals(left, right);
-    }
-
-    public static bool operator !=(Suggestion? left, Suggestion? right)
-    {
-        return !Equals(left, right);
+        return HashCode.Combine(Id, Name, Note, IconUrl, UserId, Minimum, Maximum);
     }
 }
